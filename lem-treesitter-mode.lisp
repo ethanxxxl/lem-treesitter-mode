39878d0cfddda30bebd9eed589b470d38facad92
Ethan Smith <ethansmith.dev@gmail.com>
2025-09-24 08:21:38 -0400

    created a primary treesitter parser thread that will parses every buffer with treesitter mode enabled

--- lem-treesitter-mode.lisp
+++ lem-treesitter-mode.lisp
@@ -6,7 +6,7 @@
         :lem-treesitter-mode/treesitters
         :lem-treesitter-mode/buffer
         :lem-treesitter-mode/parser)
-  (:export 
+  (:export
    #:setup
    ; re-export from filesystem.lisp
    #:*lem-cache-dir*
@@ -36,28 +36,34 @@
                                (:makefile)
                                (:ruby)))
 
-(defun parse-buffer-cb (&optional (period-ms 500))
-  (let ((buf (lem:current-buffer)))
-    (lem-treesitter-mode/buffer:parse-buffer buf
-                                             :language (lem:buffer-value buf :treesitter-lang))))
+(defparameter *treesitter-buffers* '()
+  "Buffers that are managed by PARSE-BUFFERS-THREAD.")
 
-;; WIP
-;; TODO
-;;  - create parameter that has list of treesitter buffers
-;;  - don't stop the timer until treesitter buffer list is empty
-;;  - start timer once when first treesitter minor mode is enabled
-;;  - buffers still need local variables
-;;  - parse buffer callback should work on buffer local variables (it does)
-;;    - but not on the current buffer
-;;    - parse buffer needs to apply the parse-buffer treesitter command to each
-;;      buffer that is in the treesitter buffers list
+(defconstant +treesitter-parser-timer+
+  (lem:make-timer 'parse-buffers-cb
+                  :name "Treesitter Parser")
+  "Timer that will periodically call PARSE-BUFFERS-THREAD to parse any
+treesitter buffers currently opened")
+
+;; NOTE if the the timer is already running, changing this value might
+;; not have an effect.
+(defparameter *parse-interval* 500
+  "How often treesitter will parse all the treesitter buffers.")
+
+(defun parse-buffers-thread (&optional (period-ms 500))
+  (dolist (buf *treesitter-buffers*)
+    (parse-buffer buf :language (lem:buffer-value buf :treesitter-lang))))
+
 (defun enable ()
+  "Ensures tree sitter environment is set up correctly then starts the parser
+thread."
+
   ;; ensure cache directories are setup
   (ensure-directories-exist *lem-cache-dir* :verbose t :mode #o755)
   (ensure-directories-exist *lem-treesitter-library* :verbose t :mode #o755)
-  
+
   ;; TODO autofill based on the major mode
-  ;(lem:buffer-major-mode (lem:current-buffer)) 
+  ;(lem:buffer-major-mode (lem:current-buffer))
   (let ((language (read-from-string (lem:prompt-for-string "enter language (sym): ")))
         (buf (lem:current-buffer)))
     ;(lem:message "language: ~S" language)
@@ -67,15 +73,20 @@
 
     (parse-buffer (lem:current-buffer) :language language)
 
-    (lem:message "here!")
+    ;; TODO ensure language is valid before pushing the buffer here.
+    (push (lem:current-buffer) *treesitter-buffers*)
+
     (setf (lem:buffer-value buf :treesitter-lang) language)
-        
-    ;; TODO only make a new timer if one doesn't already exist.
-    (setf (lem:buffer-value buf :treesitter-parser)
-          (lem:start-timer (lem:make-timer 'parse-buffer-cb 
-                                           :name "Treesitter Parser") 
-                           500 :repeat t))))
+
+    ;; Is there a race condition here if a buffer is started at exactly the same
+    ;; time as when the timer turns over/repeats?
+    (when (lem:timer-expired-p +treesitter-parser-timer+)
+      (lem:start-timer +treesitter-parser-timer+ *parse-interval* :repeat t))))
 
 (defun disable ()
-  (when-let (timer (lem:buffer-value (lem:current-buffer) :treesitter-parser))
-    (lem:stop-timer timer)))
+  "Removes the current buffer from the list of buffers that needs to be parse and
+stops the treesitter parse timer if there are no bufferes left to be parsed."
+  (delete (lem:current-buffer) *treesitter-buffers*)
+
+  (unless *treesitter-buffers*
+    (lem:stop-timer +treesitter-parser-timer+)))
